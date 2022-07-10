c16511c2fb1f421304a129f0bacaad22996d27e0
Igor Gabriel <contato.igorgabriel@hotmail.com>
2022-07-10 10:27:57 -0300

    Estilos

--- src/ui/theme.tsx
+++ src/ui/theme.tsx
@@ -1,10 +1,38 @@
 export const theme = {
-    color_palette: {},
+    color_palette: {
+        brand: "#0F52BA",
+        base: {
+            primary: "#F4F4F4",
+            secondary: "#373737",
+        },
+        text: {
+            primary: "#2C2C2C",
+            secondary: "#F4F4F4",
+        },
+    },
     typography: {
-        weight: {},
-        size: {},
+        weight: {
+            light: 300,
+            regular: 400,
+            semi_bold: 600,
+            bold: 700,
+        },
+        size: {
+            title: "1.125em", // 18px
+            subtitle: "15em", // 16px
+            text: "0.875em", // 14px
+            legend: "0.75em", // 12px
+        },
         line_height: "normal",
         family: "Montserrat, Helvetica, sans-serif",
     },
-    shape: {},
+    shape: {
+        border: {
+            size: "0.125rem", // 2px
+            radius: {
+                main: "0.5rem", // 8px
+                circle: "100%",
+            },
+        },
+    },
 };
